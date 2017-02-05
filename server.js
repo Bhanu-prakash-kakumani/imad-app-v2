b1682dcecc43aa74483508b225ef466b245a3295
Bhanu-prakash-kakumani <bhanuprakashk9@gmail.com>
2017-02-05 16:16:59 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -13,6 +13,15 @@ app.get('/article-one',function(req,res){
    res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
 });
 
+var counter = 0;
+app.get('/counter',function(req,res){
+    counter=counter+1;
+    res.send(counter.tostring());
+});
+
+
+
+
  app.get('/article-two',function(req,res){
     res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
 });
